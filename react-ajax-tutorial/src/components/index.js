ff9fc5173fb4c225fb7afb6789f3a892b255240d
정진배 <jb.jeong@albamapp.com>
2020-04-29 15:56:43 +0900

    react-playground init

--- react-ajax-tutorial/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Header from './Header/Header';
-import PostWrapper from './PostWrapper/PostWrapper'
-import Navigate from './Navigate/Navigate'
-
-export {
-  Header,
-  PostWrapper,
-  Navigate,
-};
